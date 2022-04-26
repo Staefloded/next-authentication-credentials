4c794cabb6563384411812c62bcacbbd8e0bd866
Michael Eleyowo <michaeleleyowo29@gmail.com>
2022-04-26 16:48:23 +0100

    added cors

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8000;
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
